ce4f38849df70cdb662d7a6b046e962d686f9cbc
Juan Carlos <jcrpelaez@yahoo.com>
2019-12-16 11:26:34 -0600

    fixed table issue

--- bamazon.sql
+++ bamazon.sql
@@ -10,9 +10,9 @@ USE bamazon_db;
 CREATE TABLE products(
     item_id INT(11) AUTO_INCREMENT NOT NULL,
     product_Name VARCHAR (100) NOT NULL, 
-    department_name VARCHAR (45) NOT NULL,
+    department_Name VARCHAR (45) NOT NULL,
     price DECIMAL(10.2) NOT NULL,
-    stock_quantity INT(11) NOT NULL,
+    stock_Quantity INT(11) NOT NULL,
     PRIMARY KEY(item_id)
 );
 
