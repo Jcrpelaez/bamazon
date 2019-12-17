654bc03b392e5957a6ad848b8148c9c1b454c1e6
Juan Carlos <jcrpelaez@yahoo.com>
2019-12-17 10:27:36 -0600

    added table function

--- bamazon.js
+++ bamazon.js
@@ -1,18 +1,18 @@
-// Uses required depedencies
-const inquirer = require("inquirer");
-const mysql = require("mysql");
-
+// loads the NPM package for inquirer and mysql
+var inquirer = require("inquirer");
+var mysql = require("mysql");
+var Table = require("cli-table2");
 // create the connection information for the sql database
-const connection = mysql.createConnection({
+var connection = mysql.createConnection({
   host: "Localhost",
 
-  // Your port; if not 3306
+  // connect the correct port
   port: 3306,
 
   // Your username
   user: "root",
 
-  // Your password
+  // Your password and database
   password: "utsabootcamp1",
   database: "bamazon_db"
 });
@@ -20,56 +20,38 @@ const connection = mysql.createConnection({
 connection.connect(function(err) {
   if (err) throw err;
   console.log("Connection successful");
-  start();
 });
 
-function start() {
-  connection.query("Select * FROM products", function(err, res) {
-    for (let i = 0; i < res.length; i++) {
-      console.log(
-        res[i].item_id +
-          " || " +
-          res[i].product_Name +
-          " || " +
-          res[i].department_Name +
-          " || " +
-          res[i].price +
-          " || " +
-          res[i].stock_Quantity +
-          "\n"
-      );
-    }
-    promptCustomer(res);
-  });
-}
-
-promptCustomer = function(res){
-  inquirer.prompt([{
-    type:"input",
-    name:'choice',
-    message:"What would you like to purchase?"
-  }]).then(function(answer){
-    let correct = false;
-    for(let i=0;i<res.length;i++){
-      if(res[i].product_Name==answer.choice){
-        correct=true;
-        let product_Name=answer.choice;
-        let id = i;
-        inquirer.prompt({
-          type:"input",
-          name:"qty",
-          message:'How many would you like to buy?',
-          validate:function(value){
-            if(isNAN(value)==false){
-              return true;
-            } else {
-              return false;
-            }
-          }
-        }).then(function(answer){
-          if((res[id]).stock_Quantity
-        }
+// function that prompts the user
+var display = function() {
+  connection.query("SELECT * FROM products", function(err, res) {
+    if (err) throw err;
+    console.log("-----------------------------");
+    console.log("      Welcome To Bamazon    ");
+    console.log("-----------------------------");
+    console.log("");
+    console.log("What would you like to buy?");
+    console.log("");
+    var table = new Table({
+      head: ["Item", "Product","Department", "Price"],
+      colWidths: [12, 30, 30, 8],
+      colAligns: ["center", "left","left", "right"],
+      style: {
+        head: ["aqua"],
+        compact: true
       }
+    });
+    for (var i = 0; i < res.length; i++) {
+      table.push([
+        res[i].item_id,
+        res[i].product_Name,
+        res[i].department_Name,
+        res[i].price
+      ]);
     }
-  })
-} 
+    console.log(table.toString());
+    console.log("");
+    shopping();
+  }); 
+};
+display();
